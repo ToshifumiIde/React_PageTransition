2207f218be9173314d77b3ff154d130fcb789ebf
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-12 22:24:04 +0900

    page transition

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Main from "./components/Main";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = ()=>{
   return(
-    <>
-    <SignUp />
-    <SignIn />
-    <Main />
-    </>
+    <Router>
+      <Switch>
+    <Route path="/signup" component={SignUp} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/main" component={Main} />
+    </Switch>
+    </Router>
   )
 }
 
